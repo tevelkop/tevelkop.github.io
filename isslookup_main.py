4144404300d7586dbe03805cef5b5457637152d0
tevelkop <125277248+tevelkop@users.noreply.github.com>
2023-11-17 23:39:30 +0200

    Update isslookup_main.py

--- isslookup_main.py
+++ isslookup_main.py
@@ -5,9 +5,8 @@ import time
 
 MY_EMAIL = "opler@gmail.com"
 MY_PASSWORD = "1424636536z"
-MY_LAT = 51.507351 # Your latitude
-MY_LONG = -0.127758 # Your longitude
-
+MY_LAT = 51.507351 
+MY_LONG = -0.127758
 
 def is_iss_overhead():
     response = requests.get(url="http://api.open-notify.org/iss-now.json")
@@ -16,8 +15,6 @@ def is_iss_overhead():
 
     iss_latitude = float(data["iss_position"]["latitude"])
     iss_longitude = float(data["iss_position"]["longitude"])
-
-    #Your position is within +5 or -5 degrees of the iss position.
     if MY_LAT-5 <= iss_latitude <= MY_LAT+5 and MY_LONG-5 <= iss_longitude <= MY_LONG+5:
         return True
 
@@ -38,8 +35,6 @@ def is_night():
 
     if time_now >= sunset or time_now <= sunrise:
         return True
-
-
 while True:
     time.sleep(60)
     if is_iss_overhead() and is_night():
