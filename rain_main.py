679a07ed7f6378b2c0c7a91e4b002288363aa0bc
tevelkop <125277248+tevelkop@users.noreply.github.com>
2023-11-17 23:55:56 +0200

    Add files via upload

---
+++ rain_main.py
@@ -0,0 +1,41 @@
+import requests
+import os
+from twilio.rest import Client
+from twilio.http.http_client import TwilioHttpClient
+
+OWM_Endpoint = "https://api.openweathermap.org/data/2.5/onecall"
+api_key = os.environ.get("OWM_API_KEY")
+account_sid = "tevelfrfrefe"
+auth_token = os.environ.get("eewewew")
+
+weather_params = {
+    "lat": "46.545",
+    "lon": "7.452",
+    "appid": api_key,
+    "exclude": "currently,minutely,hourly"
+}
+
+response = requests.get(OWM_Endpoint, params=weather_params)
+response.raise_for_status()
+weather_data = response.json()
+weather_slice = weather_data["hourly"][:12]
+
+will_rain = False
+
+for hour_data in weather_slice:
+    condition_code = hour_data["weather"][0]["id"]
+    if int(condition_code) < 700:
+        will_rain = True
+
+if will_rain:
+    proxy_client = TwilioHttpClient()
+    proxy_client.session.proxies = {'https': os.environ['https_proxy']}
+
+    client = Client(account_sid, auth_token, http_client=proxy_client)
+    message = client.messages \
+        .create(
+        body="go bring you an umbrella"
+        from_="+122531551343",
+        to="058454554541554
+    )
+    print(message.status)
