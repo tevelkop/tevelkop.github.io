462de2df08513ac5debd294b6194a43ff4f626ae
tevelkop <125277248+tevelkop@users.noreply.github.com>
2023-11-19 09:44:57 +0200

    Update hang_man.py

--- hang_man.py
+++ hang_man.py
@@ -1,4 +1,4 @@
-"""Develop a text-based hangman game where the user has to guess a random word, one letter at a time."""
+"""I Developed a text-based hangman game where the user has to guess a random word, one letter at a time."""
 
 
 def represent_found_chars(letter_from_user,word_to_find):
@@ -9,15 +9,19 @@ def represent_found_chars(letter_from_user,word_to_find):
       while(n<len(word_to_find)): 
         GUI_completed_word=GUI_completed_word+"_"
         n=n+1
+          
       GUI_completed_word[0]==""
       not_break_from_code1=False
+        
     indexes=[int(index) for index,letter in enumerate(word_to_find) if letter==letter_from_user]
     
     for i in indexes:
       GUI_completed_word = GUI_completed_word[:i] + letter_from_user + GUI_completed_word[i+1:]
+        
     if GUI_completed_word.count("_")==1:
       GUI_completed_word=GUI_completed_word.replace("_","",1)
       return GUI_completed_word
+        
     else:
       return GUI_completed_word
     
@@ -27,11 +31,15 @@ def represent_found_chars(letter_from_user,word_to_find):
 def set_all_chars_needed_to_find(word_to_find):
     global not_break_from_code2
     if not_break_from_code2==True:
+        
         global left_letters_to_win
         for char in word_to_find:
+            
           if char not in left_letters_to_win:
             left_letters_to_win += char
+              
         not_break_from_code2=False  
+        
     return left_letters_to_win
 
 
